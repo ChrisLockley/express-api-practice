db1b4a451b40631ed13e047ff08bbbc90e697f5a
Christian <christianlockley4@gmail.com>
2022-03-14 16:51:39 -0700

    made partial progress towards completing assignment; server done

--- public/index.js
+++ public/index.js
@@ -1,5 +1,10 @@
 // Using jQuery and ajax make a get request to your express server
 // Display the retrieved data on the html page
 $(document).ready(function() {
-    
-})
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        const userInput = ($('input[name="user-inp"]').val()).toLowerCase();
+    })
+
+
+});
--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 // Create an express server that will serve up your static html file
 // Create a get request in your get route to the pokeapi requesting some data
 // Return that data to the front end and display it
+
+const express = require('express');
+const app = express();
+const port = '3000'
+const path = require('path');
+
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/form-data', (req, res) => {
+    res.sendStatus(200).send({});
+})
+
+app.listen(port, function () {
+    console.log(`Server Listening On Port: ${port}`)
+});
